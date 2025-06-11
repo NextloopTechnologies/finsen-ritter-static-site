159dcaed7ac1822d901fbefdad7f9e31f56980a0
sanadkhan-nextloop <skhan@nextlooptechnologies.com>
2025-06-11 15:23:11 +0530

    Added Product Hero Section and Home page Demo Video to bucket

---
+++ src/assets/index.ts
@@ -0,0 +1,26 @@
+
+export const homeAssets = {
+  homeVideo:
+    'https://ik.imagekit.io/nextloop/FinsenRitter/Home/homepagevideo.mp4?updatedAt=1749634124959',
+ };
+
+export const productAssets = {
+  bioCng:
+    'https://ik.imagekit.io/nextloop/FinsenRitter/Product/bio-cng.jpg?updatedAt=1749634990175',
+  medicalOxygen:
+    'https://ik.imagekit.io/nextloop/FinsenRitter/Product/medical-oxygen.jpg?updatedAt=1749549805817',
+  nitrogen:
+    'https://ik.imagekit.io/nextloop/FinsenRitter/Product/nitrogen.jpg?updatedAt=1749549806131',
+  desiccantDryers:
+    'https://ik.imagekit.io/nextloop/FinsenRitter/Product/desiccant-dryers.jpg?updatedAt=1749549805845',
+  bioGas:
+    'https://ik.imagekit.io/nextloop/FinsenRitter/Product/biogas.jpg?updatedAt=1749549805757',
+  synthesisGas:
+    'https://ik.imagekit.io/nextloop/FinsenRitter/Product/synthesis-gas.jpg?updatedAt=1749549806081',
+  chillers:
+    'https://ik.imagekit.io/nextloop/FinsenRitter/Product/chillers.jpg?updatedAt=1749634041969',
+  scrubber:
+    'https://ik.imagekit.io/nextloop/FinsenRitter/Product/scrubber.jpg?updatedAt=1749634041936',
+  gasBlending:
+    'https://ik.imagekit.io/nextloop/FinsenRitter/Product/gas-blending.jpg?updatedAt=1749634042023',
+};
