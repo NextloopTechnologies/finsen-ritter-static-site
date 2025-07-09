2681d6a75838d090cb5e771ade8c88e32d1c4482
mdnizam-nextloop <mdnizam@nextlooptechnologies.com>
2025-07-09 17:54:38 +0530

    fix build issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Helmet } from "react-helmet";
 import AboutUs from "../components/AboutUs";
 import { LogoFRSVG } from "../assets/svg";
